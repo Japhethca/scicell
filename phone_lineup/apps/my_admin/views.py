0cfa85d8b43668cf190621c0e1724563ac9ba82a
japhethca <chidiebere.anyigor@gmail.com>
2018-05-05 17:35:23 +0100

    feature(admin-login): implements admin login - add admin login template and view - adds logout view

--- phone_lineup/apps/my_admin/views.py
+++ phone_lineup/apps/my_admin/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse, HttpResponseNotFound
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+
 from django.contrib.auth.models import User
 
 
 # Create your views here.
 def admin_index(request):
-    return render(request, 'my_admin/index.html')
+    print(request.GET, '<><><><><><>')
+    if request.user.is_authenticated and request.user.is_superuser:
+        return render(request, 'my_admin/admin-base-layout.html', {'user': request.user})
+    return HttpResponseRedirect('login')
 
-def create_user(request):
-    pass
+
+def admin_login(request):
+    is_authenticated = None
+
+    if request.method == 'POST':
+        user = authenticate(
+            username=request.POST['username'],
+            password=request.POST['password'],
+            )
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect('/admin')
+        else:
+            is_authenticated = False
+    return render(request, 'my_admin/login.html', {'is_authenticated': is_authenticated})
+
+def admin_logout(request):
+    logout(request)
+    return HttpResponseRedirect('login')
